0266b087ca9e273338272c4d0c11ef53a7ebb026
Mocha Adam Fadly <>
2023-11-20 15:12:10 +0800

    [auth] config

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,34 @@
+import { NextAuthOptions, getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      //   if (token) {
+      //     session.user.id = token.id;
+      //     session.user.name = token.name;
+      //     session.user.email = token.email;
+      //     session.user.image = token.picture;
+      //     session.user.username = token.username;
+      //   }
+
+      return session;
+    },
+    redirect() {
+      return "/";
+    },
+  },
+};
+export const getAuthSession = () => getServerSession(authOptions);
